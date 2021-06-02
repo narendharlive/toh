5fa72d352c19a40e31e5040b3cc9ba6a2656e1de
Narendhar <narendharlive@gmail.com>
2021-06-02 13:42:09 +0530

    Footer and main componbents are added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { mainComponent } from './shared/main/main.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent,
+    mainComponent
   ],
   imports: [
     BrowserModule
